d31a8df90b711143077524c1018347cfd60cf138
SANKALP0005 <SANKALPSINHA0005@GMAIL.COM>
2019-03-04 00:46:36 +0530

    Update solver.py

--- solver.py
+++ solver.py
@@ -2,10 +2,10 @@ def solve(xyz,K,F): #dataset,K,F
     
 	words = [""]
 	f = open("vocab."+xyz+".txt","r")
-	for w in (f.read().split()):
+	for w in (f.read().split()):#this loop add all the words in vocab to list 
 	    words.append(str(w))
 	f.close()
-	f2 = open("docword."+xyz+".txt","r")
+	f2 = open("docword."+xyz+".txt","r")#for reading the no. of docs,words,entries and wid that appears in particular docid
 	a = f2.readlines()
 	f2.close()
 
@@ -17,7 +17,7 @@ def solve(xyz,K,F): #dataset,K,F
 	for i in range(1,ndocs+1):
 	    rows.append([])
 	    
-	for i in range(3,nentries+3):
+	for i in range(3,nentries+3):#mapping words with their respective id
 	    row = a[i].split()
 	    docid = int(row[0])
 	    wordid = int(row[1])
@@ -37,10 +37,11 @@ def solve(xyz,K,F): #dataset,K,F
 	sup = (F/nentries)
 
 	frequent_itemsets = apriori(df, min_support= sup, use_colnames=True)
-	frequent_itemsets = frequent_itemsets[frequent_itemsets['itemsets'].apply(lambda x: len(x)) == K] #filter
+	frequent_itemsets = frequent_itemsets[frequent_itemsets['itemsets'].apply(lambda x: len(x)) == K] #As this pakage gives output for all possible frequent 
+	#itemset with given threshold, so apply filter to get only required k
 	#print( frequent_itemsets )
 
-	path = 'D:\\COLLEGE\\Data_set\\Output\\' + xyz + '\\K=' + str(K) + '_' + 'F=' + str(F) + '.txt' #save ouput to this file
+	path = 'D:\\COLLEGE\\Data_set\\Output\\' + xyz + '\\K=' + str(K) + '_' + 'F=' + str(F) + '.txt' #give path to save ouput to this file
 	fo = open(path,"w")
 	for index,row in frequent_itemsets.iterrows():
 	    for j in row['itemsets']:
@@ -48,7 +49,7 @@ def solve(xyz,K,F): #dataset,K,F
 	    fo.write('\n')
 	fo.close()
 	etime = time.time() - stime
-	path2 = 'D:\\COLLEGE\\Data_set\\ExecutionTimes\\' + xyz + '.txt'; #save execution time here
+	path2 = 'D:\\COLLEGE\\Data_set\\ExecutionTimes\\' + xyz + '.txt'; #give path where to save execution times 
 	ft = open(path2,"a")
 	ft.write("K=%s , F=%s --> %s sec\n" % (K,F,etime))
 	ft.close()
